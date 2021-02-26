634ccf09c3697736af67bad1769d18867b6fa81b
Soral <cx.soral@gmail.com>
2021-02-26 18:29:37 +0100

    fix: Declare Receiver at root level

--- pyinsight/__init__.py
+++ pyinsight/__init__.py
@@ -5,6 +5,7 @@ from pyinsight import loader
 from pyinsight import cleaner
 from pyinsight import merger
 from pyinsight import packager
+from pyinsight import receiver
 
 # Object Import
 from pyinsight.insight import Insight
@@ -13,6 +14,7 @@ from pyinsight.loader import Loader
 from pyinsight.cleaner import Cleaner
 from pyinsight.merger import Merger
 from pyinsight.packager import Packager
+from pyinsight.receiver import Receiver
 
 
 # Element Listing
@@ -21,6 +23,7 @@ __all__ = insight.__all__ \
     + loader.__all__ \
     + cleaner.__all__ \
     + merger.__all__ \
-    + packager.__all__
+    + packager.__all__ \
+    + receiver.__all__
 
 __version__ = "0.2.21"
